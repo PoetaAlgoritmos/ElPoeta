88fa3f10104f01fa8d93157f879fc77619b5ffdf
Joseph Salas Solano <josephsalas1112@gmail.com>
2017-11-13 00:25:12 -0600

    Core of GA

---
+++ El Poeta/PoemGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace El_Poeta
+{
+    class PoemGenerator
+    {
+         string targetString = "To be, or not to be, that is the question.";
+         string validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.|!#$%&/()=? ";
+         int populationSize = 200;
+         float mutationRate = 0.01f;
+         int elitism = 5;
+        int numCharsPerText = 15000;
+
+        private GeneticAlgorithm<char> ga;
+        private System.Random random;
+
+        void Start()
+        {
+            random = new System.Random();
+            ga = new GeneticAlgorithm<char>(populationSize, targetString.Length, random, GetRandomCharacter, FitnessFunction, elitism, mutationRate);
+        }
+
+        void Update()
+        {
+            ga.NewGeneration();
+            if (ga.BestFitness == 1)
+            {
+            }
+        }
+
+        private char GetRandomCharacter()
+        {
+            int i = random.Next(validCharacters.Length);
+            return validCharacters[i];
+        }
+
+        private float FitnessFunction(int index)
+        {
+            float score = 0;
+            DNA<char> dna = ga.Population[index];
+
+            for (int i = 0; i < dna.Genes.Length; i++)
+            {
+                if (dna.Genes[i] == targetString[i])
+                {
+                    score += 1;
+                }
+            }
+
+            score /= targetString.Length;
+
+            score = ((float)Math.Pow(2, score) - 1) / (2 - 1);
+
+            return score;
+        }
+    }
+}
